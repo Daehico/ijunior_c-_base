30873312adf3c8c51e467f212875d4bdac518020
Дмитрий Дудин <empire_95@mail.ru>
2020-05-27 21:29:25 +0300

    1 lesson
    1 lesson

---
+++ task1/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte _Byte;
+            short _12short;
+            int _int_;
+            float fantazia_na_nazvania_konchilas;
+            double _d_o_U_b_l_e_;
+            string kakoi_to_text;
+            char _char;
+            bool trueOrFalse;
+            decimal _123456decimal;
+            object Object;
+           
+        }
+    }
+}
