5869b4a25d98ae251179835b35b2e6324603078c
Дмитрий Дудин <empire_95@mail.ru>
2020-05-28 00:06:36 +0300

    task 3

---
+++ task3/task3/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countsOfPhotos = 52;
+            int countsOfPhotosInRow = 3;
+
+            int countsOfFullRow = countsOfPhotos / countsOfPhotosInRow;
+            float remainder = Convert.ToSingle(countsOfPhotos) % countsOfPhotosInRow;
+
+            Console.WriteLine("Количество заполненных рядов - " + countsOfFullRow);
+            Console.WriteLine("Картинок сверх меры - " + remainder);
+
+            Console.ReadKey();
+
+        }
+    }
+}
